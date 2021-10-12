d9765dfe215db083d2cee3b5c8a8af7a2c066b36
yoihenken <18104005@ittelkom-pwt.ac.id>
2021-10-12 11:36:57 +0700

    add loop for print number

--- src/Modul0/Try_Github.java
+++ src/Modul0/Try_Github.java
@@ -3,5 +3,8 @@ package Modul0;
 public class Try_Github {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Print number\t: " + i);
+        }
     }
 }
