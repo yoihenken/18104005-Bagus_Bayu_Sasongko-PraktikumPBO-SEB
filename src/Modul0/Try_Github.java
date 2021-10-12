154df8e47257c99b46b83d8b2f688c371eea032b
yoihenken <18104005@ittelkom-pwt.ac.id>
2021-10-12 11:38:41 +0700

    add looping text

--- src/Modul0/Try_Github.java
+++ src/Modul0/Try_Github.java
@@ -3,6 +3,11 @@ package Modul0;
 public class Try_Github {
     public static void main(String[] args) {
         System.out.println("Hello Github");
+        System.out.println("Hello Github");
+        System.out.println("Hello Github");
+        System.out.println("Hello Github");
+        System.out.println("Hello Github");
+        
         for (int i = 0; i < 10; i++) {
             System.out.println("Print number\t: " + i);
         }
---
+++ src/Modul0/Try_Github2.java
@@ -0,0 +1,9 @@
+package Modul0;
+
+public class Try_Github2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Print Text!");
+        }
+    }
+}
